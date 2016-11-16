ae35a75602e4006cdf922de1bf0f8e8b9d77088b
juan-solorzano <juan.solorzano@globant.com>
2016-11-16 16:34:15 +0530

    jenkins

---
+++ jenkins/jobs/seed.groovy
@@ -0,0 +1,30 @@
+import jenkins.model.*;
+import hudson.model.FreeStyleProject;
+import hudson.plugins.git.GitSCM;
+import hudson.tasks.Shell;
+import javaposse.jobdsl.plugin.*;
+
+def url = "https://github.com/camiloribeiro/cdeasy.git"
+def jobName = "seed"
+
+project = Jenkins.instance.createProject(FreeStyleProject, jobName)
+def gitScm = new GitSCM(url)
+gitScm.branches = [new hudson.plugins.git.BranchSpec("*/master")]
+project.scm = gitScm
+
+project.getBuildersList().clear()
+
+project.getBuildersList().add(new Shell("docker pull niaquinto/gradle"));
+
+project.getBuildersList().add(new Shell("docker run -v \$PWD:/usr/bin/app --entrypoint=gradle niaquinto/gradle build"));
+
+project.getBuildersList().add(new ExecuteDslScripts(
+  new ExecuteDslScripts.ScriptLocation("false","dsl/**/*.groovy",null),
+  false,
+  RemovedJobAction.IGNORE,
+  RemovedViewAction.IGNORE,
+  LookupStrategy.JENKINS_ROOT,
+  "src/main/groovy")
+);
+
+project.save()
